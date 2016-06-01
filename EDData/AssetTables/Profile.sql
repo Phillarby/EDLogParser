cf862e73b282bce98658317608f756bad6e2f9b5
HCRGroup <phil@larby.org>
2016-06-01 10:11:13 +0100

    Profile update

--- EDData/AssetTables/Profile.sql
+++ EDData/AssetTables/Profile.sql
@@ -3,8 +3,9 @@
 	[Id] INT NOT NULL PRIMARY KEY, 
     [Commander] NVARCHAR(150) NOT NULL, 
     [Email] NVARCHAR(150) NOT NULL, 
+	[EmailValidated] BIT NOT NULL DEFAULT(0),
     [Password] NVARCHAR(256) NOT NULL, 
-    [SecurityToken] NVARCHAR(50) NULL, 
-    [TokenExpiry] DATETIME NULL, 
+    [SecurityToken] NVARCHAR(50) NOT NULL, 
+    [TokenExpiry] DATETIME NOT NULL, 
     CONSTRAINT FK_Profile_Asset FOREIGN KEY (Id) REFERENCES [EDDATA].[Asset] (Id)
 )
---
+++ EDData/SProcs/AddLogFile.sql
@@ -0,0 +1,48 @@
+﻿CREATE PROCEDURE [EDData].[AddLogFile]
+	@Id int OUTPUT,
+	@ProfileID INT, 
+    @Filename VARCHAR(50), 
+	@FileDate DateTime, 
+    @LastParse DateTime, 
+    @LinesParsed INT
+    
+AS
+	BEGIN TRY
+		BEGIN TRAN
+		
+			DECLARE @LogFileAssetType INT,
+					@Created DateTime
+			SELECT  @LogFileAssetType = 2, 
+					@Created = GetDate()
+		
+			--Create a new Asset to associated with the new log file
+			EXEC EDData.AddAsset 
+				@Id = @Id OUTPUT, 
+				@AssetTypeID = @LogFileAssetType,  
+				@Description = @Filename, 
+				@ParentID = @ProfileID
+
+			--Create logfile Record
+			INSERT INTO [EDDATA].[LogFile] (
+				[Id],
+				[ProfileID], 
+				[LogfileName], 
+				[LogFileDate],
+				[LastParsed],
+				[LinesParsed]
+			) VALUES (
+				@Id,
+				@ProfileID, 
+				@FileName, 
+				@FileDate,
+				@LastParse,
+				@LinesParsed
+			)
+
+		COMMIT TRAN
+	END TRY
+
+	BEGIN CATCH
+	IF @@TRANCOUNT > 0 
+		ROLLBACK
+	END CATCH
